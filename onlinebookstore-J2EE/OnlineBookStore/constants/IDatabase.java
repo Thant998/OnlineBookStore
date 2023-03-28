811e553d74ec1d2f0eb51eecbe2898de8861bc88
OG Bobby <95152361+Thant998@users.noreply.github.com>
2023-03-28 12:42:40 +0630

    Update IDatabase.java

--- onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
+++ onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
@@ -5,6 +5,6 @@ public class IDatabase {
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
 	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
 	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root";
+	public static final String PASSWORD = "sHj@6378#jw";
 
 }
