1e993473dc3d6695ebbe5b45e7d94a394a2c6dc3
Minnth Ant <95152361+Thant998@users.noreply.github.com>
2023-03-31 11:35:14 +0630

    Update IOnlineBookStoreConstants.java

--- onlinebookstore-J2EE/OnlineBookStore/constants/IOnlineBookStoreConstants.java
+++ onlinebookstore-J2EE/OnlineBookStore/constants/IOnlineBookStoreConstants.java
@@ -1,7 +1,5 @@
 package constants;
 
 public interface IOnlineBookStoreConstants {
-	public static String CONTENT_TYPE_TEXT_HTML = "text/html";
-	
-	
+	public static String CONTENT_TYPE_TEXT_HTML = "text/html";	
 }
